e917a90575559c6ffc2f429ce98c94d0485eb381
James Vo <ebtskin@gmail.com>
2020-06-05 20:07:26 -0700

    Fix navBar issues

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,38 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import "jquery/dist/jquery";
 import "bootstrap/dist/css/bootstrap.css";
 
-const NavBar = () => {
-  return (
-    <React.Fragment>
-      <nav className="navbar navbar-light bg-secondary justify-content-between fixed-top">
-        <a className="navbar-brand mx-auto text-white" href="/" alt="logo">
-          Traffic Playback
-        </a>
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-light bg-secondary justify-content-between fixed-top">
+          <a className="navbar-brand mx-auto text-white" href="/" alt="logo">
+            Traffic Playback
+          </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#nav-items"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#nav-items"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="nav-items">
-          <div className="navbar-nav ml-auto">
-            <a className="nav-item nav-link" href="/login">
-              Login <span className="sr-only">(current)</span>
-            </a>
-            <a className="nav-item nav-link" href="/signUp">
-              Sign Up
-            </a>
+          <div className="collapse navbar-collapse" id="nav-items">
+            <div className="navbar-nav ml-auto">
+              <a className="nav-item nav-link" href="/login">
+                Login <span className="sr-only">(current)</span>
+              </a>
+              <a className="nav-item nav-link" href="/signUp">
+                Sign Up
+              </a>
+            </div>
           </div>
-        </div>
-      </nav>
-    </React.Fragment>
-  );
-};
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
 
 export default NavBar;
+

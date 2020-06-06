576f4fba2fdfcf1af0b9d60204be14e704409894
James Vo <ebtskin@gmail.com>
2020-06-05 19:39:07 -0700

    nav bar and doughnut chart

---
+++ src/components/doughnutChart.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Doughnut } from "react-chartjs-2";
+import Axios from "axios";
+
+class DoughnutChart extends React.Component {
+  state = {
+    labels: ["GET", "POST", "HEAD"],
+    options: {
+      responsive: false,
+    },
+    legend: {
+      position: "bottom",
+    },
+  };
+
+  async componentDidMount() {
+    const datasets = [
+      {
+        data: [1, 2, 3],
+        backgroundColor: ["#5bc0de", "#d9534f", "#5cb85c"],
+      },
+    ];
+
+    const { data: captureDomains } = await Axios.get(
+      "http://ec2-54-152-230-158.compute-1.amazonaws.com:7999/api/play"
+    );
+    this.setState({ captureDomains });
+
+    captureDomains.forEach((element) => {
+      if (element.method === "GET") ++datasets[0].data[0];
+      else if (element.method === "POST") ++datasets[0].data[1];
+      else if (element.method === "HEAD") ++datasets[0].data[2];
+    });
+    this.setState({ datasets });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h3 className="text-center">
+          <small class="text-muted">Capture Methods</small>
+        </h3>
+        <Doughnut data={this.state} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default DoughnutChart;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const NavBar = () => {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-light bg-secondary justify-content-between fixed-top">
+        <a className="navbar-brand mx-auto text-white" href="/" alt="logo">
+          Traffic Playback
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#nav-items"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="nav-items">
+          <div className="navbar-nav ml-auto">
+            <a className="nav-item nav-link" href="/login">
+              Login <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link" href="/signUp">
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;

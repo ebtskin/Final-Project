402cc47d1957c63ad044235f8c080423905f0218
James Vo <ebtskin@gmail.com>
2020-06-06 00:37:20 -0700

    heroku debugging

--- src/App.js
+++ src/App.js
@@ -19,47 +19,9 @@ class App extends Component {
             <div className="col-md-3 bg-light fixed-top">
               <NavBar />
             </div>
-
-            <div className="col-md-9 offset-3">
-              <br />
-              <br />
-
-              <Stats />
-              <br />
-              <br />
-
-              <div class="card bg-primary text-white text-center p-3">
-                <blockquote class="blockquote mb-0">
-                  <DateUI />
-                </blockquote>
-              </div>
-              <br />
-              <br />
-              <div class="card bg-info text-white text-center p-3">
-                <blockquote class="blockquote mb-0 text-white">
-                  <h3 className="text-center text-white">Playback Jobs</h3>
-                </blockquote>
-              </div>
-
-              <Playback />
-              <br />
-              <br />
-              <div class="card bg-success text-white text-center p-3">
-                <blockquote class="blockquote mb-0 text-white">
-                  <h3 className="text-center text-white">Capture Jobs</h3>
-                </blockquote>
-              </div>
-              <CaptureJob />
-              <br />
-              <br />
-              <div class="card bg-dark text-white text-center p-3">
-                <blockquote class="blockquote mb-0 text-white">
-                  <h3 className="text-center text-white">Real Time Traffic</h3>
-                </blockquote>
-              </div>
-            </div>
           </div>
         </main>
+
       </React.Fragment>
     );
   }

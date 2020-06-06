3da5e5e9b2fa5ae3bea475b88a02615e52aad135
James Vo <ebtskin@gmail.com>
2020-06-05 20:20:59 -0700

    document usage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navBar"; //top navbar
+import SideBar from "./components/sideBar"; //side navbar
+import Stats from "./components/stats"; //bootstrap card stats
+import Chart from "./components/doughnutChart"; //chartjs capture method
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Playback from "./components/playback"; //playback traffic
+import CaptureJob from "./components/captureJobs"; //capture traffic
+import DateUI from "./components/date"; //date picker
+import ReatTime from "./components/realTime"; //real time traffic monitoring
+
+//Main app component
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main className="container">
+          <div className="row">
+            <div className="col-md-3 bg-light fixed-top">
+              <NavBar />
+              <SideBar />
+              <Chart />
+            </div>
+
+            <div className="col-md-9 offset-3">
+              <br />
+              <br />
+
+              <Stats />
+              <br />
+              <br />
+
+              <div class="card bg-primary text-white text-center p-3">
+                <blockquote class="blockquote mb-0">
+                  <DateUI />
+                </blockquote>
+              </div>
+              <br />
+              <br />
+              <div class="card bg-info text-white text-center p-3">
+                <blockquote class="blockquote mb-0 text-white">
+                  <h3 className="text-center text-white">Playback Jobs</h3>
+                </blockquote>
+              </div>
+
+              <Playback />
+              <br />
+              <br />
+              <div class="card bg-success text-white text-center p-3">
+                <blockquote class="blockquote mb-0 text-white">
+                  <h3 className="text-center text-white">Capture Jobs</h3>
+                </blockquote>
+              </div>
+              <CaptureJob />
+              <br />
+              <br />
+              <div class="card bg-dark text-white text-center p-3">
+                <blockquote class="blockquote mb-0 text-white">
+                  <h3 className="text-center text-white">Real Time Traffic</h3>
+                </blockquote>
+              </div>
+              <ReatTime />
+            </div>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
+
